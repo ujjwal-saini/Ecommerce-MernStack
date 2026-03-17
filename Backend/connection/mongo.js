7d8c07affed51ae560e39888e2d2490a748f15d5
Ujjwal <ujjwalsaini073@gmail.com>
2026-03-17 17:13:52 +0530

    added global API URL

--- Backend/connection/mongo.js
+++ Backend/connection/mongo.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
 
-
 const connectDB = async () => {
-
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log("MongoDB connection error:", error);
+    console.log("MongoDB connection error:", error.message);
   }
 };
 
 export default connectDB;
-
