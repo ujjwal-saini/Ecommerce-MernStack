4f364531f3205e4859ab0b2ed5af457ff33b6fb0
Ujjwal <ujjwalsaini073@gmail.com>
2026-03-21 23:10:33 +0530

    fixed api and cors issue

--- Frontend/src/middleware/ProtectedRoute.jsx
+++ Frontend/src/middleware/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "./authContext";
+import Loader from "../components/loading";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <h3><Loader/></h3>;
 
   if (user && user.role==="admin") {
     return <Navigate to="/admindashboard" />;
