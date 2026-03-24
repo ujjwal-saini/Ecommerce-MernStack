f127d8fa28e229f316ea95ccd88e31190a417a8c
Ujjwal <ujjwalsaini073@gmail.com>
2026-03-24 22:45:45 +0530

    fixed api and cors issue

--- Backend/server.js
+++ Backend/server.js
@@ -2,31 +2,28 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from "cloudinary";
-import path from "path";
-
+import {v2 as cloudinary} from "cloudinary"
 import connectDB from "./connection/mongo.js";
 import Routes from "./routes/route.js";
 
 dotenv.config();
 
+cloudinary.config({
+  cloud_name:process.env.cloudinary_cloud_name,
+  api_key:process.env.cloudinary_api_key,
+  api_secret:process.env.cloudinary_api_secret
+});
+
+
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
-// cloudinary
-cloudinary.config({
-  cloud_name: process.env.cloudinary_cloud_name,
-  api_key: process.env.cloudinary_api_key,
-  api_secret: process.env.cloudinary_api_secret
-});
-
-// middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
+// console.log(process.env.FrontendUrl);
 app.use(
   cors({
     origin: process.env.FrontendUrl,
@@ -39,17 +36,8 @@ app.use("/uploads", express.static("uploads"));
 // DB
 connectDB();
 
-// API routes
-app.use("/api", Routes);
-
-
-
-app.use(express.static(path.join(__dirname, "Frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Frontend/build", "index.html"));
-});
-
+// routes
+app.use("/", Routes);
 
 // server
 app.listen(PORT, () => {
