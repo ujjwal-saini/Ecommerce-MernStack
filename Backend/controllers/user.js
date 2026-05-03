c5475dee58d2dd378b87ae5a7398bea960d5ff48
Ujjwal <ujjwalsaini073@gmail.com>
2026-05-03 14:18:42 +0530

    fixed api and cors issue

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -30,7 +30,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "true",
+      sameSite: "false",
       maxAge: 24 * 2 * 60 * 60 * 1000,
 
     });
